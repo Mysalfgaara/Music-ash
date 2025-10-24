f80e6603b0a0c0f431c850280c052256834f6897
Nexxababy <kartik18210@gmail.com>
2025-10-24 14:19:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,9 +40,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ───── Git & Updates ───── #
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/nexxababy/Meri-palak")
+UPSTREAM_REPO = "https://github.com/nexxababy/Meri-palak"
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
-GIT_TOKEN = getenv("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")
+GIT_TOKEN = "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"
 
 # ───── Support & Community ───── #
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/The_Moon_Network")
